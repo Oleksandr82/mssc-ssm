000cf202e2887679ae8cfa87aa71e9e38291bf9b
Oleksandr Sytnik <Oleksandr.Sytnik@rabobank.nl>
2020-07-26 17:01:13 +0200

    WIP: Updated the service to restore a state of the state machine from DB.

--- src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
+++ src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import guru.springframework.msscssm.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,16 +26,35 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuthorize(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuthorization(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+        Payment payment = paymentRepository.getOne(paymentId);
+
+        StateMachine<PaymentState, PaymentEvent> sm =
+                stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+        sm.stop();
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(sma -> sma.resetStateMachine(
+                        new DefaultStateMachineContext<>(payment.getState(), null, null, null)));
+        sm.start();
+        return sm;
     }
 }
